df08142db935800ba653748edd02975f75ee6629
matsufriends <pikapikan0526@gmail.com>
2026-01-11 00:08:59 +0900

    MornTransitionCtrlを非Inject化

--- EnterSceneState.cs
+++ EnterSceneState.cs
@@ -8,7 +8,6 @@ namespace MornLib
 {
     public class EnterSceneState : StateBehaviour
     {
-        [Inject] private MornTransitionCtrl _transitionCtrl;
         [Inject] private MornSoundVolumeCore _volumeCore;
         [SerializeField, Label("独立動作")] private bool _isExecuteAsIsolated;
         [SerializeField] private StateLink _nextState;
@@ -16,7 +15,7 @@ namespace MornLib
         public override async void OnStateBegin()
         {
             var ct = _isExecuteAsIsolated ? MornApp.QuitToken : CancellationTokenOnEnd;
-            var taskA = _transitionCtrl.ClearAsync(ct);
+            var taskA = MornTransitionService.ClearAsync(ct);
             var taskB = _volumeCore.FadeAsync(MornDirectorGlobal.I.CreateFadeInInfo(ct));
             await UniTask.WhenAll(taskA, taskB);
             Transition(_nextState);
--- ExitSceneState.cs
+++ ExitSceneState.cs
@@ -8,7 +8,6 @@ namespace MornLib
 {
     public class ExitSceneState : StateBehaviour
     {
-        [Inject] private MornTransitionCtrl _transitionCtrl;
         [Inject] private MornSoundVolumeCore _volumeCore;
         [SerializeField, Label("独立動作")] private bool _isExecuteAsIsolated;
         [SerializeField, Label("トランジション")] private MornTransitionType _transitionType;
@@ -17,7 +16,7 @@ namespace MornLib
         public override async void OnStateBegin()
         {
             var ct = _isExecuteAsIsolated ? MornApp.QuitToken : CancellationTokenOnEnd;
-            var taskA = _transitionCtrl.FillAsync(_transitionType, ct);
+            var taskA = MornTransitionService.FillAsync(_transitionType, ct);
             var taskB = _volumeCore.FadeAsync(MornDirectorGlobal.I.CreateFadeOutInfo(ct));
             await UniTask.WhenAll(taskA, taskB);
             Transition(_nextState);
